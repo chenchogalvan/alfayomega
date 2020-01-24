f5e36babaadd8663ff0e3f440ed443192cb327c0
Alfredo Galvan <alfredogalvan.91@gmail.com>
2020-01-24 17:09:47 -0600

    AyO Facebook

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -351,10 +351,10 @@
       <!-- Your customer chat code -->
       <div class="fb-customerchat"
         attribution=setup_tool
-        page_id="778478712199357"
-  theme_color="#ff7e29"
-  logged_in_greeting="¡Hola! ¿En que podemos ayudarte? "
-  logged_out_greeting="¡Hola! ¿En que podemos ayudarte? ">
+			page_id="778478712199357"
+		<!--theme_color="#ff7e29"-->
+		logged_in_greeting="¡Hola! ¿En que podemos ayudarte? "
+		logged_out_greeting="¡Hola! ¿En que podemos ayudarte? ">
       </div>
 </body>
 
