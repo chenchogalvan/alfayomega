0435af621831bd0381b2fdbd111009753cca4eb0
Alfredo Galvan <alfredogalvan.91@gmail.com>
2020-01-24 17:20:10 -0600

    AyO Facebook

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -331,19 +331,28 @@
 
 	
 	<!-- Load Facebook SDK for JavaScript -->
-	<script>
-	(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v5.0'
+          });
+        };
 
-	</script>
-	<div class="fb-customerchat"
-	 page_id="778478712199357">
-	</div>
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your customer chat code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="778478712199357">
+      </div>
      
 </body>
 
