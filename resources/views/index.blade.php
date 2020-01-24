0881f2af100552d0de555435c34053366dafda58
Alfredo Galvan <alfredogalvan.91@gmail.com>
2020-01-24 17:51:44 -0600

    AyO Facebook

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -354,9 +354,9 @@
         attribution=setup_tool
 			page_id="778478712199357"
 		<!--theme_color="#ff7e29"-->
-		
 		logged_in_greeting="¡Hola! ¿En que podemos ayudarte? "
-		logged_out_greeting="¡Hola! ¿En que podemos ayudarte? ">
+		logged_out_greeting="¡Hola! ¿En que podemos ayudarte? "
+		greeting_dialog_display="hide">
       </div>
 </body>
 
