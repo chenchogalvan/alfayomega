592ae7e2e1fa4f30ab3332e4f207622f69f7d5d4
Alfredo Galvan <alfredogalvan.91@gmail.com>
2020-01-24 17:57:41 -0600

    AyO Facebook

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -332,32 +332,32 @@
 	
 	<!-- Load Facebook SDK for JavaScript -->
 
-      <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v5.0'
-          });
-        };
+      <!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v5.0'
+  });
+};
 
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
-      <!-- Your customer chat code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-			page_id="778478712199357"
-		<!--theme_color="#ff7e29"-->
-		logged_in_greeting="¡Hola! ¿En que podemos ayudarte? "
-		logged_out_greeting="¡Hola! ¿En que podemos ayudarte? "
-		greeting_dialog_display=hide>
-      </div>
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=install_email
+  page_id="778478712199357"
+  theme_color="#ff7e29"
+    logged_in_greeting="¿HOLA EN QUE PODEMOS AYUDARTE?"
+    logged_out_greeting="¿HOLA EN QUE PODEMOS AYUDARTE?">
+</div>
 </body>
 
 </html>
