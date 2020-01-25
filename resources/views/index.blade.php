5c8195d0a35dafc91ed017c7b77db526cef32f44
Alfredo Galvan <alfredogalvan.91@gmail.com>
2020-01-24 18:12:17 -0600

    AyO Facebook

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -354,9 +354,6 @@ window.fbAsyncInit = function() {
 <div class="fb-customerchat"
   attribution=install_email
   page_id="778478712199357"
-  <!--theme_color="#ff7e29"-->
-    <!--logged_in_greeting="¿HOLA EN QUE PODEMOS AYUDARTE?"
-    logged_out_greeting="¿HOLA EN QUE PODEMOS AYUDARTE?"-->
 	greeting_dialog_display="hide">
 </div>
 </body>
